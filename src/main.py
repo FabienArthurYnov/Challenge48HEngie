bf561c4070affb3fe9dcf42dccf84c6af911539b
Your Name <you@example.com>
2025-03-10 16:16:15 +0100

    func agent

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from reader import Reader
 from plots import *
 from writer import Writer
+from agent import ask_agent
 
 reader = Reader('./ressources/input/filtered_tweets_engie.csv')
 messages = reader.load_messages()
@@ -26,3 +27,5 @@ msg_per_date([dt.date() for dt in all_datetime])
 word_occurence_graph(word_occurence)
 writer = Writer("./ressources/generated/full_cleaned_msg.csv")
 writer.write_from_array_obj(messages)
+
+ask_agent(messages)
