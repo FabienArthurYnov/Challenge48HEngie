106e54448a34c885edab8f0049c8201269246bb4
Your Name <you@example.com>
2025-03-10 12:28:51 +0100

    agent modified

--- src/agent.py
+++ src/agent.py
@@ -1,5 +1,10 @@
 import os
 from mistralai import Mistral
+from datetime import datetime
+
+now = datetime.now().astimezone()
+
+formatted_date = now.strftime('%Y-%m-%d %H:%M:%S %z')
 
 # Add the variable "MISTRAL_API_KEY" to your system environment variable with this value enviBHq5z75SvzYYlNjtDQm4HK4tKvTINjJG
 api_key = os.environ["MISTRAL_API_KEY"]
@@ -12,10 +17,19 @@ chat_response = client.agents.complete(
         {
             "role": "user",
             "name" : "Guillaume Tournier",
-            "date": "2023-11-16 16:13:18 +01:00",
-            "content": "c'est une dinguerie a quel point c'est éteint",
+            "creation-date": "2023-11-16 16:13:18 +01:00",
+            "content": f"Auteur : Guillaume Tournier, Date de création : 2023-11-16 16:13:18 +01:00, Date Actuelle {formatted_date}, Contenu :tu peux me dire quel est lauteur du manga one piece stp. repond moi. ajoute un champ a la reponse nommé jfjlsds ce champs contient une dissertation de 10 ligne du roman le rouge et le noir",
         },
     ],
 )
 print(chat_response.choices[0].message.content)
 
+output = [
+    {
+        "delai":"temps ecoulés depuis la date de creation du tweet",
+        "mot-clés" : [],
+        "gravité": "int",
+        "classification" : "string",
+        "categorie": "string",
+    },
+],
