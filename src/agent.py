b9b855ac97316995884a5288aad85d53b9461336
Your Name <you@example.com>
2025-03-10 10:51:11 +0100

    added the 1st version of our ia agent

---
+++ src/agent.py
@@ -0,0 +1,20 @@
+import os
+from mistralai import Mistral
+
+api_key = os.environ["MISTRAL_API_KEY"]
+
+client = Mistral(api_key=api_key)
+
+chat_response = client.agents.complete(
+    agent_id= "ag:96b775aa:20250310:chall48h:07bbfff0",
+    messages=[
+        {
+            "role": "user",
+            "name" : "Guillaume Tournier",
+            "date": "2023-11-16 16:13:18 +01:00",
+            "content": "c'est une dinguerie a quel point c'est éteint",
+        },
+    ],
+)
+print(chat_response.choices[0].message.content)
+
